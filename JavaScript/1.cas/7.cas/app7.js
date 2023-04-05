a9a0709623bbedbbc17308a9d7011aa97a83f929
anastasijamilovic <anastasija.milovic02@gmail.com>
2023-04-05 20:21:49 +0200

    JavaScript

--- JavaScript/1.cas/7.cas/app7.js
+++ JavaScript/1.cas/7.cas/app7.js
@@ -91,3 +91,19 @@ var suma = 0;
 for(var i = 10; i <= 20; i += 1){
     if(i % 2 == 1){suma += i}
 }
+
+// 1
+for(let i = 1; i <= 20; 1 --){
+    if( i % 2 === 0){
+        console.log(i)
+    }
+}
+for(let i = 2; i <= 20; i -= 2){
+    console.log(i)
+}
+
+// 2
+for(let i = 50; i <= 100; 1 ++){
+    if(i % 5 === 0)
+console.log(i);
+}
