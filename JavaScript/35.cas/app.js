26f3213558e87de40779cc0ad2a7451689aeb86c
anastasijamilovic <anastasija.milovic02@gmail.com>
2023-06-30 11:47:42 +0200

    JavaScrpt

--- JavaScript/35.cas/app.js
+++ JavaScript/35.cas/app.js
@@ -45,3 +45,19 @@ console.log(fruits.includes("pineapple"));
 
 const arr = Array.from("ABCDEF");
 console.log(arr);
+
+/* Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. 
+For example if you accept 025468 the output should be 0-254-6-8. */
+
+const dashes = (num) => {
+  let newNum = "";
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0 && num[i + 1] % 2 === 0) {
+      newNum += num[i] + "-";
+    } else {
+      newNum += num[i];
+    }
+  }
+  return newNum;
+};
+console.log(dashes("025468"));
