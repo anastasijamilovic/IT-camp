ae15e1c19b9ae2976ca35b3220f708f682f3e85f
anastasijamilovic <anastasija.milovic02@gmail.com>
2023-07-17 21:04:15 +0200

    JavaScript

--- JavaScript/41.cas/app.js
+++ JavaScript/41.cas/app.js
@@ -52,3 +52,15 @@ ne mozemo koristiti niz koji se tek kreira u uslovu same callback funkcije*/
   return newArr;
 };
 console.log(removeDuplicates(fruits));
+
+// Domaci:
+// 1. Na osnovu stringa koji unosi korisnik napraviti niz, gde cemo ispitati da li se jagoda nalazi unutar tog niza. Ako se jagoda ne nalazi unutar niza, funkcija neka vrati taj niz poredjan abecedno, s tim sto ce se na prvom mestu dodati broj 10, a na poslednjem broj 100.
+// Dok ako se jagoda nalazi unutar niza vratiti poziciju gde se ona nalazi.
+
+// 2. We have the following arrays : Go to the editor
+color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+o = ["st", "nd", "rd", "th"];
+// Write a JavaScript program to display the colors in the following way :
+// "1st choice is Blue."
+// "2nd choice is Green."
+// "3rd choice is Red."
